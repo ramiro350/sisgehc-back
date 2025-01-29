5fc89c23abd60623ec607abe83efcba7e8e1eedb
Vinicius de Moraes Menezes <132612286+Luckiestaxe@users.noreply.github.com>
2025-01-29 15:04:29 -0300

    Update serializer.py

--- sisgehc/serializer.py
+++ sisgehc/serializer.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from sisgehc.models import Aluno, Curso, Evento, Professor, Inscricao
+from sisgehc.models import Aluno, Curso, Evento, Professor, Inscricao, CoordenadorModel, Presenca, AtividadeComplementar
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['nome', 'horasComplementares', 'curso', 'matricula', 'senha']
+        fields = ['id_matricula', 'nome', 'horasComplementares', 'curso','senha']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,10 +20,31 @@ class EventoSerializer(serializers.ModelSerializer):
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
-        fields = ['nome', 'matricula', 'senha', 'curso']
+        fields = ['id_matricula','nome','senha', 'curso']
 
 
 class InscricaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inscricao
-        fields = ['id','evento', 'Aluno']
+        fields = ['id_inscricao','evento', 'aluno']
+
+class CoordenadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoordenadorModel
+        fields = ['id_matricula', 'nome', 'senha', 'curso']
+
+class PresencaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Presenca
+        fields = ['id_presenca','inscricao']
+
+class ACSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AtividadeComplementar
+        fields = ['id_atividade',
+                  'aluno',
+                  'carga_horaria',
+                  'coordenador',
+                  'data_submissao',
+                  'data_validacao'
+                  ]
