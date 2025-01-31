df25a3b96466f1764a8dadfdd90090eab32390ba
Yago Melo da Costa <107082028+Melinh0@users.noreply.github.com>
2025-01-31 10:45:38 -0300

    Update admin.py - Commit 31/01/2025:10:45
    Adicionei models que não estavam inseridos antes.
    Ex: AtividadeComplementar

--- sisgehc/admin.py
+++ sisgehc/admin.py
@@ -1,45 +1,52 @@
 from django.contrib import admin
-
-from sisgehc.models import Aluno, Curso, Professor, Evento, Inscricao, CoordenadorModel
+from sisgehc.models import Aluno, Curso, Professor, Coordenador, Evento, Inscricao, AtividadeComplementar
 
 class Alunos(admin.ModelAdmin):
-    list_display = ('id_matricula','nome','senha', 'horas_complementares', 'curso')
+    list_display = ('id_matricula', 'nome', 'horas_complementares', 'curso')
     list_display_links = ('id_matricula', 'nome')
     search_fields = ('nome',)
     list_per_page = 20
 admin.site.register(Aluno, Alunos)
 
-class Cursos (admin.ModelAdmin):
-    list_display = ('id_curso','nome')
+class Cursos(admin.ModelAdmin):
+    list_display = ('id_curso', 'nome')
     list_display_links = ('id_curso', 'nome')
     search_fields = ('nome',)
     list_per_page = 20
 admin.site.register(Curso, Cursos)
 
 class Professores(admin.ModelAdmin):
-    list_display = ('id_matricula','nome', 'curso', 'senha')
+    list_display = ('id_matricula', 'nome', 'curso', 'senha')
     list_display_links = ('id_matricula', 'nome')
     search_fields = ('nome',)
     list_per_page = 20
 admin.site.register(Professor, Professores)
 
-class Coordenador(admin.ModelAdmin):
-    list_display = ('id_matricula','nome', 'curso','senha')
-    list_display_links = ('id_matricula','nome')
-    seach_fields = ('nome',)
-admin.site.register(CoordenadorModel, Coordenador)
+class Coordenadores(admin.ModelAdmin):
+    list_display = ('id_matricula', 'nome', 'curso', 'senha')
+    list_display_links = ('id_matricula', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+admin.site.register(Coordenador, Coordenadores)
 
-class Eventos (admin.ModelAdmin):
-    list_display = ('id_evento','nome', 'professor', 'curso', 'tipo','horas_complementares', 'data_inicio', 'data_encerramento', 'hora_inicio', 'hora_encerrado', 'limite_inscricao','descricao_evento')
+class Eventos(admin.ModelAdmin):
+    list_display = ('id_evento', 'nome', 'professor', 'curso', 'tipo', 'horas_complementares', 
+                    'data_inicio', 'data_encerramento', 'hora_inicio', 'hora_encerrado', 'descricao_evento')
     list_display_links = ('id_evento', 'nome')
     search_fields = ('nome',)
     list_per_page = 20
 admin.site.register(Evento, Eventos)
 
-class Inscricoes (admin.ModelAdmin):
-    list_display = ('id_inscricao','evento', 'aluno')
+class Inscricoes(admin.ModelAdmin):
+    list_display = ('id_inscricao', 'evento', 'aluno', 'participacao_boolean', 'porcent_participacao')
     list_display_links = ('id_inscricao',)
-    search_fields = ('evento',)
+    search_fields = ('evento__nome', 'aluno__nome')
     list_per_page = 20
 admin.site.register(Inscricao, Inscricoes)
 
+class AtividadesComplementares(admin.ModelAdmin):
+    list_display = ('id_atividade', 'aluno', 'carga_horaria', 'coordenador', 'data_submissao', 'tipo_atividade', 'area_de_conhecimento')
+    list_display_links = ('id_atividade',)
+    search_fields = ('aluno__nome', 'tipo_atividade')
+    list_per_page = 20
+admin.site.register(AtividadeComplementar, AtividadesComplementares)
