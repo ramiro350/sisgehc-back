bb4ab804f6c6619bcff6c8f03b4c0cf886b5be60
Yago Melo da Costa <107082028+Melinh0@users.noreply.github.com>
2025-01-31 10:42:39 -0300

    Update views.py - Commit 31/01/25:10:42
    Adicionei alguns serializers n inseridos antes

--- sisgehc/views.py
+++ sisgehc/views.py
@@ -1,12 +1,10 @@
-from rest_framework import viewsets, generics
-from django.shortcuts import render
-from sisgehc.models import Aluno, Curso, Evento, Professor, Inscricao
-from sisgehc.serializer import AlunoSerializer, CursoSerializer, EventoSerializer, ProfessorSerializer, InscricaoSerializer
-
+from rest_framework import viewsets
+from sisgehc.models import Aluno, Curso, Evento, Professor, Inscricao, AtividadeComplementar, Coordenador
+from sisgehc.serializer import AlunoSerializer, CursoSerializer, EventoSerializer, CoordenadorSerializer, ProfessorSerializer, InscricaoSerializer, ACSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
-    queryset = Aluno.objects.all() 
+    queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
 
 class CursosViewSet(viewsets.ModelViewSet):
@@ -19,14 +17,22 @@ class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
 
-
 class ProfessorViewSet(viewsets.ModelViewSet):
     """Exibindo todos os professores"""
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
-
 class InscricaoViewSet(viewsets.ModelViewSet):
-    """Exibindo todos as Inscricoes"""
+    """Exibindo todas as inscrições"""
     queryset = Inscricao.objects.all()
     serializer_class = InscricaoSerializer
+
+class CoordenadorViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as atividades complementares"""
+    queryset = Coordenador.objects.all()
+    serializer_class = CoordenadorSerializer
+
+class AtividadeComplementarViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as atividades complementares"""
+    queryset = AtividadeComplementar.objects.all()
+    serializer_class = ACSerializer
